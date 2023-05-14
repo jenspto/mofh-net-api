ae74b5ceb47bb6d39843a0e06effe6f99f438c68
Pto Buon <ptobuon@outlook.it>
2023-05-14 20:26:47 +0200

    Security patch

---
+++ source/Accounts.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace MOFH_api
+{
+    public class Accounts
+    {
+        public static async void Create(string apiUsername, string apiPassword, string username, string password, string contactemail, string domain, string plan)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/createacct.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("username=" + username + "&password=" + password + "&contactemail=" + contactemail + "&domain=" + domain + "&plan=" + plan);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while creating account: " + ex.Message);
+            }
+        }
+        public static async void Suspend(string apiUsername, string apiPassword, string user, string reason)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/suspendacct.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+
+                request.Content = new StringContent("user=" + user + "&reason=" + reason);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while suspending account: " + ex.Message);
+            }
+        }
+        public static async void Unsuspend(string apiUsername, string apiPassword, string user)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/unsuspendacct.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("user=" + user);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while unsuspending account: " + ex.Message);
+            }
+        }
+        public static async void ChangePassword(string apiUsername, string apiPassword, string user, string password)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/passwd.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("user=" + user + "&pass=" + password);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while changing password: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ source/Domains.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace MOFH_api
+{
+    public class Domains
+    {
+        public static async void CheckAvailable(string apiUsername, string apiPassword, string domain)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/checkavailable.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("api_user=" + apiUsername + "&api_key=" + apiPassword + "&domain=" + domain);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while checking domain: " + ex.Message);
+            }
+        }
+        public static async void GetUserDomains(string apiUsername, string apiPassword, string username)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/getuserdomains.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("api_user=" + username + "&api_key=" + apiPassword + "&username=" + username);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception getting user domains: " + ex.Message);
+            }
+        }
+        public static async void GetUserByDomain(string apiUsername, string apiPassword, string domain)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                using var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.myownfreehost.net/xml-api/getdomainuser.php");
+                var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiUsername + ":" + apiPassword));
+                request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+                request.Content = new StringContent("api_user=" + domain + "&api_key=" + apiPassword + "&username=" + domain);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while getting user by domain: " + ex.Message);
+            }
+        }
+    }
+}
